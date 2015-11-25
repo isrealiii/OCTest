81971e6b8207f06f899505a6387c7fef0f7d1daf
Isreal <Isreal@isrealdemacbook-pro.local>
2015-11-25 23:35:08 +0800

    2015-11-25 Code

--- L01-code/L01-code/main.m
+++ L01-code/L01-code/main.m
@@ -1,17 +1,42 @@
 //
 //  main.m
-//  L01-code
+//  OCTest
 //
 //  Created by Isreal on 15/11/25.
 //  Copyright © 2015年 Isreal. All rights reserved.
 //
 
+#import "Fraction.h"
 #import <Foundation/Foundation.h>
 
 int main(int argc, const char * argv[]) {
+    
+    
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+        
+        printf("hello world\n");
+        
+        Fraction *frac = [[Fraction alloc] init];
+        Fraction *frac2 = [[Fraction alloc] init];
+        Fraction *frac3 = [[Fraction alloc] initNumber:100 number2:500];
+        
+        [frac setN1: 1];
+        [frac setN2: 5];
+        
+        [frac2 setNumber:10 number2:50];
+        
+        printf("The fraction is: ");
+        
+        [frac print];
+        
+        printf("\nfraction2 is: ");
+        
+        [frac2 print];
+        
+        printf("\nfraction3 is: ");
+        
+        [frac3 print];
+        
+        return 0;
     }
-    return 0;
 }
