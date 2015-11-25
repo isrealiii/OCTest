34dc160151a75037eef6e2cee9570e905a524622
Isreal <Isreal@isrealdemacbook-pro.local>
2015-11-25 23:23:08 +0800

    first commit

---
+++ L01-code/L01-code/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  L01-code
+//
+//  Created by Isreal on 15/11/25.
+//  Copyright © 2015年 Isreal. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
